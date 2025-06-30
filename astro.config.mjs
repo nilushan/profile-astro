9671a11fb8c21fa735d4097aafbeb4fd8b0a3e5b
nilushan <nilushan.silva@gmail.com>
2025-06-30 23:09:42 +1000

    feat: Integrate Starlight Tailwind for enhanced styling and remove custom Starlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,30 +14,31 @@ export default defineConfig({
   integrations: [
     react(),
     // Starlight docs mounted at /docs/
-    starlight(
-      {
-        title: "Knowledge Base",
+    // starlight(
+    //   {
+    //     title: "Knowledge Base",
 
-        // head: [
-        //   // Example: add Fathom analytics script tag.
-        //   {
-        //     tag: 'script',
-        //     attrs: {
-        //       src: 'https://cdn.usefathom.com/script.js',
-        //       'data-site': 'MY-FATHOM-ID',
-        //       defer: true,
-        //     },
-        //   },
-        // ],
-        sidebar:[
-          {label: 'Home', link: 'docs'},
-          {label: 'Welcome' , link: 'welcome' },
-          {label: 'Architecture' , autogenerate: { directory: 'kb/architecture'}},
-          {label: 'Cloud', autogenerate: {directory: 'kb/cloud'}  },
-          {label: 'Guides', autogenerate: { directory: 'kb/guides'}}
-        ]
-      },
-    ),
+    //     // head: [
+    //     //   // Example: add Fathom analytics script tag.
+    //     //   {
+    //     //     tag: 'script',
+    //     //     attrs: {
+    //     //       src: 'https://cdn.usefathom.com/script.js',
+    //     //       'data-site': 'MY-FATHOM-ID',
+    //     //       defer: true,
+    //     //     },
+    //     //   },
+    //     // ],
+    //     sidebar:[
+    //       {label: 'Home', link: 'docs'},
+    //       {label: 'Welcome' , link: 'welcome' },
+    //       {label: 'Architecture' , autogenerate: { directory: 'kb/architecture'}},
+    //       {label: 'Cloud', autogenerate: {directory: 'kb/cloud'}  },
+    //       {label: 'Guides', autogenerate: { directory: 'kb/guides'}}
+    //     ],
+    //     customCss:['./src/styles/global.css']
+    //   },
+    // ),
     mdx(),
   ]
 });
